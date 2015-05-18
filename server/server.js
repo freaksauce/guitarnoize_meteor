cdbf55a9c4df208355e473b36041fb00ffccf2ff
Jon Bloomer <jon@freaksauce.com>
2015-05-18 23:02:16 +1000

    implemented upsert finallygit add .

--- server/server.js
+++ server/server.js
@@ -5,12 +5,20 @@ Meteor.methods({
       // console.log(post.title);
       myID = post.ID;
       myTitle = post.title;
-console.log(myID);
-      var myInsert = posts.insert({
-        ID: myID,
-        title: myTitle
+      myContent = post.content;
+console.log(post);
+      // var myInsert = Posts.insert({
+      //   ID: myID,
+      //   title: myTitle
+      // });
+      Posts.upsert({ID: myID}, {
+        // Modifier
+        $set: {
+            ID: myID,
+            title: myTitle,
+            content: myContent
+        }
       });
-      console.log(myInsert);
 
     });
   }
