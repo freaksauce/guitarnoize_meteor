7b598dd66bde75b07eee2ad41cfa68381827029b
Jon Bloomer <jon@freaksauce.com>
2015-05-20 22:37:31 +1000

    minor UI animation updates

--- client/client.js
+++ client/client.js
@@ -1,9 +1,27 @@
 Meteor.subscribe("posts");
 
+function aniLogo() {
+  $('.logo').animate({
+    top: '120px',
+    width: '150px',
+    height: '110px',
+    marginLeft: '-75px'
+  }, {
+    duration:600,
+    easing:'easeOutExpo',
+    complete: function() {
+      Meteor.setTimeout(function() {
+        Router.go('/home');
+      }, 200);
+    }
+  });
+}
+
 Template.loadingTemplate.onRendered(function() {
+  $('.logo').addClass('show');
   Meteor.setTimeout(function() {
-    $('.logo').addClass('show');
-  }, 100);
+    aniLogo();
+  }, 1000);
 });
 
 Template.defaultLayout.helpers({
